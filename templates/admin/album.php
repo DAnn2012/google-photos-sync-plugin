d43332d55f9de58c5a55afe38f31dd98811342c5
DAnn2012 <dannunziata@hotmail.it>
2025-10-05 11:45:52 +0200

    Update album.php

--- templates/admin/album.php
+++ templates/admin/album.php
@@ -1,6 +1,6 @@
 <div class="wrap">
 	<?php settings_errors(); ?>
-	<h1><?php esc_html( get_admin_page_title() ); ?></h1>
+	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 	<form action="options.php" method="post">
 		<?php
 			do_settings_sections( 'google_photos_sync_album' );
